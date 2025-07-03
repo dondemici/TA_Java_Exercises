3114bfbc8533eaea2f8799fa0aefe22514df66c1
dondemici <acyantune@gmail.com>
2025-07-03 10:50:21 -0700

    more wrapper

--- WrapperClassDemo.java
+++ WrapperClassDemo.java
@@ -6,8 +6,20 @@ public class WrapperClassDemo {
         Float num4=Float.valueOf(2.3f);
         System.out.println(num1);
         Float num5=Float.valueOf("2.3s");
-        Float num6=Double.valueOf("Dundee");
+        //Float num6=Double.valueOf("Dundee");
         
         // this will compile but will run into an exception (number format exception)
+
+        int num7 = num1 + num2; //implicit unboxing
+        String password="Cengage@123";
+        char[]password_chars=password.toCharArray();
+        int count=0;
+        if(password.length()>=8 && password.length()<=16){
+            for (int i = 0; i<password_chars.length;i++){
+                if(Character.isLetter(password_chars[i]))
+                count++;
+            }
+            System.out.println("");
+        }
     }
 }
